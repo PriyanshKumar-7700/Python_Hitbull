dac9fba57c9be3f08e29bc55515075ac4730a477
PriyanshKumar-7700 <priyanshkumar21jul@gmail.com>
2026-04-23 19:15:06 +0530

    commit

---
+++ Graph.py
@@ -0,0 +1,14 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def __str__(self):
+        return str(self.graph)
